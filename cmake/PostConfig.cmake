d754e7024c30e1793ff0d3b336d6627bc4c2625a
Adriano Amaricci <adriano.amaricci@gmail.com>
2025-03-17 15:22:40 +0100

    Fixed install messagge

--- cmake/PostConfig.cmake
+++ cmake/PostConfig.cmake
@@ -171,25 +171,25 @@ $ ${BARE_MAKE_PROGRAM} test
 MESSAGE( STATUS "${Red}Library version:${ColourReset} ${VERSION}")
 MESSAGE( STATUS "${Red}Library will be installed in:${ColourReset} ${CMAKE_INSTALL_PREFIX}")
 
-# INSTALL(CODE "MESSAGE(
-# \"
-# ADD LIBRARY TO YOUR SYSTEM: 
-# Pick ONE method below [or add it in your bash profile, e.g. ~/.bashrc]:
-# ${Yellow}Method 1: use the provided ${PROJECT_NAME} environment module${ColourReset}:
-#    $ module use $HOME/.modules.d
-#    $ module load ${TMP_MODULE_NAME}
+INSTALL(CODE "MESSAGE(
+\"
+ADD LIBRARY TO YOUR SYSTEM: 
+Pick ONE method below [or add it in your bash profile, e.g. ~/.bashrc]:
+${Yellow}Method 1: use the provided ${PROJECT_NAME} environment module${ColourReset}:
+   $ module use $HOME/.modules.d
+   $ module load ${TMP_MODULE_NAME}
 
-# ${Yellow}Method 2: source the config script${ColourReset}:
-#    $ source ${LIB_TARGET_BIN}/${PROJECT_NAME}_config_user.sh
+${Yellow}Method 2: source the config script${ColourReset}:
+   $ source ${LIB_TARGET_BIN}/${PROJECT_NAME}_config_user.sh
 
-# ${Yellow}Method 3: use pkg-config with the provided ${PROJECT_NAME}.pc${ColourReset}:
-#    $ export PKG_CONFIG_PATH=${LIB_TARGET_ETC}/:$PKG_CONFIG_PATH
-#    $ pkg-config --cflags --libs ${PROJECT_NAME}
+${Yellow}Method 3: use pkg-config with the provided ${PROJECT_NAME}.pc${ColourReset}:
+   $ export PKG_CONFIG_PATH=${LIB_TARGET_ETC}/:$PKG_CONFIG_PATH
+   $ pkg-config --cflags --libs ${PROJECT_NAME}
 
-# ${Yellow}Method ADMIN: Add this line to the system shell configuration file, e.g. /etc/bash.bashrc${ColourReset}
-#    $ source ${LIB_TARGET_BIN}/${PROJECT_NAME}_config_global.sh
-# \")
-# ")
+${Yellow}Method ADMIN: Add this line to the system shell configuration file, e.g. /etc/bash.bashrc${ColourReset}
+   $ source ${LIB_TARGET_BIN}/${PROJECT_NAME}_config_global.sh
+\")
+")
 
 
 
