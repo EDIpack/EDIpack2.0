39f46d58897ea566097ccb5e49ae12026194c2b6
lcrippa <crippa.lorenzo@gmail.com>
2025-03-24 21:55:49 +0100

    Removed ED_VERSION

--- src/singlesite/ED_VERSION.f90
+++
@@ -1,8 +0,0 @@
-MODULE ED_VERSION
-  implicit none
-  !GIT VERSION
-  character(len=41),parameter,public :: version = "17a7eee9950127e69a661ce6170479df1c018599"
-  contains			 !this is a silly trick to avoid the ar no symbol issues in OSX
-  subroutine foo_ed_version_foo()
-  end subroutine foo_ed_version_foo
-END MODULE ED_VERSION
