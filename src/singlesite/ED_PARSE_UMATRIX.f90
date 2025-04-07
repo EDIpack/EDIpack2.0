ed8bfc21e749eaa3e9d257cc1a26545b6cc58184
lcrippa <crippa.lorenzo@gmail.com>
2025-04-07 17:16:05 +0200

    Don't run workflow on branches that are not master

---
+++ src/singlesite/ED_PARSE_UMATRIX.f90
@@ -0,0 +1,37 @@
+MODULE ED_PARSE_UMATRIX
+  !:synopsis: Routines and types for bath-impurity sparse maps
+  USE SF_IOTOOLS, only: str,free_unit
+  USE ED_VARS_GLOBAL
+  USE ED_INPUT_VARS
+  implicit none
+  private
+  
+  
+  subroutine read_umatrix_file
+  
+  
+  
+  end subroutine read_umatrix_file
+  
+  
+  
+  subroutine normal_order
+  
+  
+  end subroutine normal_order
+  
+  
+  subroutine update_interaction_params
+  
+  
+  end subroutine update_interaction_params
+
+
+
+end module ED_PARSE_UMATRIX
+
+
+
+
+
+
