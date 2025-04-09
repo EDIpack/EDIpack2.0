d6472600190b975bde4395e303c35a59d464aa35
lcrippa <crippa.lorenzo@gmail.com>
2025-04-09 15:34:45 +0200

    Intermediate commit
    Use allocatable string in order to allow for random length inputfile
    name in c_bindings. Thanks to @alexkowalski

--- src/c_bindings/edipack2ineq/edipack2ineq_c_binding_read_input.f90
+++ src/c_bindings/edipack2ineq/edipack2ineq_c_binding_read_input.f90
@@ -2,18 +2,20 @@
 subroutine ed_read_input_c(instr) bind(c, name='read_input')
     use, intrinsic :: iso_c_binding
     character(kind=c_char), dimension(*), intent(IN) :: instr
-    character(len=20), allocatable :: INPUTunit
+    character(len=:), allocatable :: INPUTunit_fortran
     integer :: length
     integer :: i
     length=0
-    INPUTunit=" "
     do
        if (instr(length+1) == C_NULL_CHAR) exit
        length = length + 1
     end do
+    if(allocated(INPUTunit_fortran))deallocate(INPUTunit_fortran)
+    allocate(character(len=length) :: INPUTunit_fortran)
     do i = 1, length
-      INPUTunit(i:i) = instr(i)
+      INPUTunit_fortran(i:i) = instr(i)
     enddo
-    INPUTunit=trim(INPUTunit)
-    call ed_read_input(INPUTunit) 
+    INPUTunit_fortran=trim(INPUTunit_fortran)
+    call ed_read_input(INPUTunit_fortran) 
+    deallocate(INPUTunit_fortran)
 end subroutine ed_read_input_c
