4fb678f22c17cce2bb6965289b2cc601e8eeb104
lcrippa <crippa.lorenzo@gmail.com>
2025-04-10 18:28:11 +0200

    Progressed writing parsing function for umatrix file

--- src/singlesite/ED_PARSE_UMATRIX.f90
+++ src/singlesite/ED_PARSE_UMATRIX.f90
@@ -31,7 +31,7 @@ contains
   subroutine read_umatrix_file(ufile)
     !This subroutine reads the interaction Hamiltonian from a user-specified file.
     !It would make sense for the elements to be ordered in this way:
-    !cdg_s cdg_sprime c_s c_sprime.
+    !cd_s cd_sprime c_s c_sprime.
     character(len=*) :: ufile  !File containing a properly formatted interaction Hamiltonian
     logical          :: master=.true.,ufile_exists, verbose
     integer          :: iline,flen,unit_umatrix,rank
@@ -51,6 +51,14 @@ contains
     if(.not.ufile_exists)stop "read_dmft_bath ERROR: indicated file does not exist" !#FIXME: change this to make it default back to Uloc&co.
     if(ed_verbose>2)write(LOGfile,"(A)")'Reading interaction Hamiltonian from file '//str(ufile)
     if(ed_verbose>2)write(LOGfile,"(A)")'There are '//str(flen)//' two-body operators.'
+    !
+    !Set internal interaction coefficient matrices to zero
+    mfHloc        = zero
+    Uloc_internal = zero
+    Ust_internal  = zero
+    Jh_internal   = zero
+    Jx_internal   = zero
+    Jp_internal   = zero
     !
     flen = file_length(str(ufile),verbose=ed_verbose>2)
     !
@@ -73,8 +81,8 @@ contains
        coulombmatrix%oplist(iline)%c_l  = [o4, merge(1, 2, s4 == "u")]
        if(ed_verbose>4)write(LOGfile,"(A)")'Two-body operator '//str(iline)//' '//&
                                              str(coulombmatrix%oplist(iline)%U)//&
-                                           ' cdg_['//str(o1)//str(s1)//']'&
-                                           ' cdg_['//str(o2)//str(s2)//']'&
+                                           ' cd_['//str(o1)//str(s1)//']'&
+                                           ' cd_['//str(o2)//str(s2)//']'&
                                            ' c_['//str(o3)//str(s3)//']'&
                                            ' c_['//str(o4)//str(s4)//']'
     enddo
@@ -86,59 +94,89 @@ contains
   
   
   subroutine parse_umatrix_line(line)
-  !This funcion switches the second and third elements of the coulomb matrix applying
-  !commutation relations. If the resulting series of operators is of the type that would
-  !couple to Uloc, Ust, Ust-Jh, Jx, Jp, store the resulting coefficient in the appropriate
-  !matrix .
-  !If some mean-field terms result from the anticommutation, store them in a mean-field 
-  !matrix to be added along with impHloc at Fock space H creation time.
-  !If a Coulomb matrix element comes along that would be inconsistent with the previously
-  !determined value of Uloc, Ust, Ust-Jh, Jx, Jp then set the previously determined coeff
-  !to zero and add this to the list of "everything_else".
-  type(coulomb_matrix_element)        :: line
-  character(len=10)                   :: operator_type
-  integer,dimension(2)                :: dummy
-
-  !First: order the two creation operators so that they
-  !are set in an increasing order of (first) spin and (second) orbital from left to right
-  !
-  !spin
-  if (line%cd_i(2) > line%cd_j(2)) then
-    dummy = line%cd_i
-    line%cd_i = line%cd_j
-    line%cd_j = dummy
-    line%U    = -1.0*line%U
-  !orbital
-  elseif (line%cd_i(1) > line%cd_j(1)) then
-    dummy = line%cd_i
-    line%cd_i = line%cd_j
-    line%cd_j = dummy
-    line%U    = -1.0*line%U
-  endif
-  !
-  !Second: order the two annihilation operators so that they
-  !are set in an increasing order of (first) spin and (second) orbital from left to right
-  !
-  !spin
-  if (line%c_i(2) > line%c_j(2)) then
-    dummy = line%c_i
-    line%c_i = line%c_j
-    line%c_j = dummy
-    line%U    = -1.0*line%U
-  !orbital
-  elseif (line%c_i(1) > line%c_j(1)) then
-    dummy = line%c_i
-    line%c_i = line%c_j
-    line%c_j = dummy
-    line%U    = -1.0*line%U
-  endif
-  !
-  !Third: multiply U by (-1) because operators will always be applied from left to right
-  !as c->cdg->c->cdg, so second and third element are to be swapped
-  
-  
-  
-  
+    !This funcion switches the second and third elements of the coulomb matrix applying
+    !commutation relations. If the resulting series of operators is of the type that would
+    !couple to Uloc, Ust, Ust-Jh, Jx, Jp, store the resulting coefficient in the appropriate
+    !matrix .
+    !If some mean-field terms result from the anticommutation, store them in a mean-field 
+    !matrix to be added along with impHloc at Fock space H creation time.
+    !If a Coulomb matrix element comes along that would be inconsistent with the previously
+    !determined value of Uloc, Ust, Ust-Jh, Jx, Jp then set the previously determined coeff
+    !to zero and add this to the list of "everything_else".
+    type(coulomb_matrix_element)        :: line
+    character(len=10)                   :: operator_type
+    integer,dimension(2)                :: dummy
+    
+    !First: order the two creation operators so that they
+    !are set in an increasing order of (first) spin and (second) orbital from left to right
+    !
+    !spin
+    if (line%cd_i(2) > line%cd_j(2)) then
+      dummy = line%cd_i
+      line%cd_i = line%cd_j
+      line%cd_j = dummy
+      line%U    = -1.0*line%U
+    !orbital
+    elseif (line%cd_i(1) > line%cd_j(1)) then
+      dummy = line%cd_i
+      line%cd_i = line%cd_j
+      line%cd_j = dummy
+      line%U    = -1.0*line%U
+    endif
+    !
+    !Second: order the two annihilation operators so that they
+    !are set in an increasing order of (first) spin and (second) orbital from left to right
+    !
+    !spin
+    if (line%c_k(2) > line%c_l(2)) then
+      dummy = line%c_k
+      line%c_k = line%c_l
+      line%c_l = dummy
+      line%U    = -1.0*line%U
+    !orbital
+    elseif (line%c_k(1) > line%c_l(1)) then
+      dummy = line%c_k
+      line%c_k = line%c_l
+      line%c_l = dummy
+      line%U    = -1.0*line%U
+    endif
+    !
+    !
+    !Third: are the indices of the swapped operators the same? If so, there is a mean-field
+    !term coming out of the anticommutator
+    if(all((line%cd_j==line%c_k)))then
+      mfHloc(line%cd_i(2),line%c_k(2),line%cd_i(1),line%c_k(1)) = mfHloc(line%cd_i(2),line%c_k(2),line%cd_i(1),line%c_k(1)) + line%U
+    endif  
+    !
+    !Fourth: multiply U by (-1) because operators will always be applied from right to left
+    !as c->cd->c->cd, so second and third element are to be swapped
+    line%U = -1.0 * line%U
+    !
+    !Fifth: look at the four-operator term. Does it look like any of the ones we would put 
+    !in an Hubbard-Kanamori density-density interaction?
+    if(line%cd_i(1)==line%c_k(1))then          !c_dag c: First two operators form a density
+      if(line%cd_j(1)==line%c_l(1))then        !c_dag c: Second two operators form a density
+        if(line%cd_i(2)/=line%cd_j(2))then          !c_dag spin are opposite: It is either Uloc or Ust
+          if(line%cd_i(1)==line%cd_j(1))then        !It is Uloc
+            Uloc_internal(line%cd_i(1)) = Uloc_internal(line%cd_i(1)) + line%U
+          else    !It is Ust
+            Ust_internal(line%cd_i(1),line%cd_j(1)) = Ust_internal(line%cd_i(1),line%cd_j(1)) + line%U
+          endif
+        else
+          if(line%cd_i(1)/=line%cd_j(1))then        !It is Ust - Jh. Store this coefficient in Jh for now. Will need to do Jh = Ust - this in the end.
+            Jh_internal(line%cd_i(1),line%cd_j(1)) = Jh_internal(line%cd_i(1),line%cd_j(1)) + line%U
+          endif
+        endif
+      endif
+    endif
+    !
+    !Sixth: is it spin-flip or pair-hopping?
+          
+      
+    
+    
+    
+    
   end subroutine parse_umatrix_line
   
   
